595552bb5a3a62fb67a246dbdc4523a7ee121319
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2025-12-10 23:38:02 -0300

    feat: Add Navbar component for site navigation; integrate into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Schibsted_Grotesk, Martian_Mono } from 'next/font/google';
 import './globals.css';
 import LightRays from '@/components/LightRays';
+import Navbar from '@/components/Navbar';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: '--font-schibsted-grotesk',
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang='en'>
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}>
+        <Navbar />
         <div className='top-0 z-[-1] absolute inset-0'>
           <LightRays
             raysOrigin='top-center-offset'
