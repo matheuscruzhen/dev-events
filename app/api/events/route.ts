5f9f05d8fd0efd83d9ef587e0e47a3b81e965e2b
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2026-02-16 16:35:59 -0300

    feat: add get events route

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/mongodb';
 import Event from '@/database/event.model';
+import { v2 as cloudinary } from 'cloudinary';
 
 export async function POST(req: NextRequest) {
   try {
@@ -18,6 +19,36 @@ export async function POST(req: NextRequest) {
         { status: 400 },
       );
     }
+
+    const file = formData.get('image') as File;
+
+    if (!file) {
+      return NextResponse.json(
+        { message: 'Image file is required' },
+        { status: 400 },
+      );
+    }
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: 'image',
+            folder: 'DevEvent',
+          },
+          (error, results) => {
+            if (error) return reject(error);
+
+            resolve(results);
+          },
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
 
     const createdEvent = await Event.create(event);
 
@@ -37,6 +68,24 @@ export async function POST(req: NextRequest) {
         message: 'Event Creation Failed',
         error: error instanceof Error ? error.message : 'Unknown',
       },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { message: 'Events fetched succesfully', events },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Event fetching failed', error },
       { status: 500 },
     );
   }
